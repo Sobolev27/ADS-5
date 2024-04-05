459482172a6a8034404f9c224710801cb56ad31a
Sobolev27 <ale.sobolev.52@gmail.com>
2024-04-05 13:50:30 +0300

    b218

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,48 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+#include <iostream>
+#include <stdexcept>
+
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T *data;
+    int top;
+public:
+    TStack() : top(-1) {
+        data = new T[size];
+    }
+
+    ~TStack() {
+        delete[] data;
+    }
+
+    void push(const T &item) {
+        if (top <= size - 1)
+            data[top++] = item;
+        else
+            throw std::string("Full");
+    }
+
+    T pop() {
+        if (top > 0)
+            return data[--top];
+        else
+            throw std::string("Empty!");
+    }
+
+    bool isEmpty() {
+        return top == 0;
+    }
+
+    T watch() {
+        if (top < 0) {
+            throw std::string("Underflow");
+        }
+        return data[top];
+    }
+
 };
 
 #endif  // INCLUDE_TSTACK_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,82 @@
 // Copyright 2021 NNTU-CS
 #include <string>
-#include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+bool operation(char iChar) {
+    return (iChar == '*' || iChar == '/' || iChar == '-' || iChar == '+');
 }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+int priority(char iOper) {
+    if (iOper == '*' || iOper == '/') return 2;
+    if (iOper == '+' || iOper == '-') return 1;
+    else return 0;
+}
+
+std::string infx2pstfx(const std::string inf) {
+    std::string postf;
+    TStack<char, 100> stack1;
+    for (int i = 0; i < inf.size(); i++) {
+        char iChar = inf[i];
+        if (isdigit(iChar)) {
+            int operand = 0;
+            while (i < inf.size() && isdigit(inf[i])) {
+                operand = operand * 10 + (inf[i] - '0');
+                i++;
+            }
+            i--;
+            postf += std::to_string(operand) + " ";
+        } else if (isalpha(iChar)) {
+            postf += iChar;
+            postf += " ";
+        } else if (iChar == '(') {
+            stack1.push(iChar);
+        } else if (iChar == ')') {
+            while (!stack1.isEmpty() && stack1.watch() != '(') {
+                postf += stack1.pop();
+                postf += ' ';
+            }
+            if (!stack1.isEmpty() && stack1.watch() == '(') {
+                stack1.pop();
+            }
+        } else if (operation(iChar)) {
+            while (!stack1.isEmpty() && priority(stack1.watch()) >= priority(iChar)) {
+                postf += stack1.pop();
+                postf += ' ';
+            }
+            stack1.push(iChar);
+        }
+    }
+    while (!stack1.isEmpty()) {
+        postf += stack1.pop();
+        postf += ' ';
+    }
+    return postf;
+}
+
+int eval(const std::string pref) {
+    TStack<int, 100> stack2;
+    for (int i = 0; i < pref.size(); i++) {
+        if (isdigit(pref[i])) {
+            int operand = 0;
+            while (i < pref.size() && isdigit(pref[i])) {
+                operand = operand * 10 + (pref[i] - '0');
+                i++;
+            }
+            i--;
+            stack2.push(operand);
+        } else if (operation(pref[i])) {
+            int iOper2 = stack2.pop();
+            int iOper1 = stack2.pop();
+            if (pref[i] == '+') {
+                stack2.push(iOper1 + iOper2);
+            } else if (pref[i] == '-') {
+                stack2.push(iOper1 - iOper2);
+            } else if (pref[i] == '*') {
+                stack2.push(iOper1 * iOper2);
+            } else if (pref[i] == '/') {
+                stack2.push(iOper1 / iOper2);
+            }
+        }
+    }
+    return stack2.pop();
 }
